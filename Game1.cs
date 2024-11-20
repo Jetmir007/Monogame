1e5bc043aac8ad5f098838393612cb4e9bc8c001
Jetmir007 <jeku07001@utb.vaxjo.se>
2024-11-20 11:08:22 +0100

    Pong 1

---
+++ Game1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    Texture2D pixel;
+
+    Rectangle paddleLeft = new Rectangle(10, 200, 20, 100);
+
+    Rectangle paddleRight = new Rectangle(770, 200, 20, 100);
+
+    Rectangle ball = new Rectangle(390, 230, 20, 20);
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        pixel = Content.Load<Texture2D>(assetName: "pixel");
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(pixel, paddleLeft, Color.DeepPink);
+        _spriteBatch.Draw(pixel, paddleRight, Color.DeepPink);
+        _spriteBatch.Draw(pixel, ball, Color.Red);
+        _spriteBatch.End();
+        base.Draw(gameTime);
+    }
+}
